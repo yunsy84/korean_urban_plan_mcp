d28699b86d315e643e5591f4b708600972098494
yunsy84 <lsmxoe10@gmail.com>
2026-09-24 20:53:49 +0900

    Use EUM notice detail sequence when building notice URL

--- src/eum_source_client.js
+++ src/eum_source_client.js
@@ -880,7 +880,11 @@ export function buildNoticeDetailUrl(
 
   url.searchParams.set(
     "seq",
-    ""
+    String(
+      notice.seq ??
+      notice.wtnnc_cd ??
+      ""
+    )
   );
 
   url.searchParams.set(
